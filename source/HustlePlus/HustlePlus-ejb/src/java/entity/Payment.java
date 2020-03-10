1c39023ef8c02fac7f215fd3ad9e6b5b7a5ab779
Damien Tan <dtjldamien@gmail.com>
2020-03-10 21:25:12 +0800

    created adminstaff and payment entities

---
+++ source/HustlePlus/HustlePlus-ejb/src/java/entity/Payment.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author dtjldamien
+ */
+@Entity
+public class Payment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+    @NotNull
+    @Size(max = 30, message = "Account name should be at most 30 characters")
+    @Column(nullable = false, length = 30)
+    private String accountName;
+    @NotNull
+    @Column(nullable = false)
+    private Long accountNumber;
+    // optional payment description
+    @NotNull
+    @Column(nullable = false)
+    private String paymentDescription;
+    @Column(nullable = false)
+    @NotNull
+    private Boolean paid;
+
+    @ManyToOne(optional = true, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = true)
+    private Milestone milestone;
+
+    public Payment(String accountName, Long accountNumber, String paymentDescription) {
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.paymentDescription = paymentDescription;
+        this.paid = Boolean.FALSE;
+    }
+
+    public Long getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(Long paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getPaymentDescription() {
+        return paymentDescription;
+    }
+
+    public void setPaymentDescription(String paymentDescription) {
+        this.paymentDescription = paymentDescription;
+    }
+
+    public Boolean getPaid() {
+        return paid;
+    }
+
+    public void setPaid(Boolean paid) {
+        this.paid = paid;
+    }
+
+    public Milestone getMilestone() {
+        return milestone;
+    }
+
+    public void setMilestone(Milestone milestone) {
+        this.milestone = milestone;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (paymentId != null ? paymentId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the paymentId fields are not set
+        if (!(object instanceof Payment)) {
+            return false;
+        }
+        Payment other = (Payment) object;
+        if ((this.paymentId == null && other.paymentId != null) || (this.paymentId != null && !this.paymentId.equals(other.paymentId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Payment[ id=" + paymentId + " ]";
+    }
+
+}
