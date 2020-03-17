dc99d9fafa616b67de0a994b4c92e97c08264f0a
Amanda_Lai <laityamanda@gmail.com>
2020-03-17 20:51:49 +0800

    filter students by skills use case

---
+++ source/HustlePlus/HustlePlus-war/src/java/jsf/managedBean/FilterStudentsBySkillsManagedBean.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedBean;
+
+import ejb.session.stateless.StudentSessionBeanLocal;
+import entity.Student;
+import java.io.IOException;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author amanda
+ */
+@Named(value = "filterStudentsBySkillsManagedBean")
+@RequestScoped
+public class FilterStudentsBySkillsManagedBean {
+
+    @EJB(name = "StudentSessionBeanLocal")
+    private StudentSessionBeanLocal studentSessionBeanLocal;
+    
+    @Inject
+    private ViewStudentManagedBean viewStudentManagedBean;
+    
+    private String condition;
+    private List<Long> selectedSkillIds;
+    private List<SelectItem> selectItems;
+    private List<Student> students;
+
+    /**
+     * Creates a new instance of FilterStudentsBySkillsManagedBean
+     */
+    public FilterStudentsBySkillsManagedBean() {
+        condition = "OR";
+    }
+    
+    @PostConstruct
+    public void postConstruct()
+    {
+        /*
+        List<Skill> skills = skillSessionBeanLocal.retrieveAllSkills();
+        selectItems = new ArrayList<>();
+        
+        for(Skill s:skills)
+        {
+            selectItems.add(new SelectItem(s.getSkillId(), s.getName(), s.getName()));
+        }
+        
+        
+        condition = (String)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("studentFilterCondition");        
+        selectedSkillIds = (List<Long>)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("studentFilterSkills");
+        */
+        
+        filterStudent();
+    }
+    
+    @PreDestroy
+    public void preDestroy()
+    {
+        
+    }
+    
+    public void filterStudent()
+    {        
+        /*
+        if(selectedSkillIds != null && selectedSkillIds.size() > 0)
+        {
+            students = studentSessionBeanLocal.filterStudentsBySkills(selectedSkillIds, condition);
+        }
+        else
+        {
+            students = studentSessionBeanLocal.retrieveAllProducts();
+        }
+        */
+    }
+    
+    public void viewStudentDetails(ActionEvent event) throws IOException
+    {
+        Long studentIdToView = (Long)event.getComponent().getAttributes().get("studentId");
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().put("studentIdToView", studentIdToView);
+        FacesContext.getCurrentInstance().getExternalContext().getFlash().put("backMode", "filterStudentBySkills");
+        FacesContext.getCurrentInstance().getExternalContext().redirect("viewStudent.xhtml");
+    }
+
+    /**
+     * @return the selectedSkillIds
+     */
+    public List<Long> getSelectedSkillIds() {
+        return selectedSkillIds;
+    }
+
+    /**
+     * @param selectedSkillIds the selectedSkillIds to set
+     */
+    public void setSelectedSkillIds(List<Long> selectedSkillIds) {
+        this.selectedSkillIds = selectedSkillIds;
+    }
+
+    /**
+     * @return the selectItems
+     */
+    public List<SelectItem> getSelectItems() {
+        return selectItems;
+    }
+
+    /**
+     * @param selectItems the selectItems to set
+     */
+    public void setSelectItems(List<SelectItem> selectItems) {
+        this.selectItems = selectItems;
+    }
+
+    /**
+     * @return the students
+     */
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    /**
+     * @param students the students to set
+     */
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    /**
+     * @return the condition
+     */
+    public String getCondition() {
+        return condition;
+    }
+
+    /**
+     * @param condition the condition to set
+     */
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+    
+}
