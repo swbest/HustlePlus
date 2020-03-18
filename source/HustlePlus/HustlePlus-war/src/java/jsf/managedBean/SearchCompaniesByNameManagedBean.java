9c024a0ae927335195af7a76b152ed86b97efb80
Amanda_Lai <laityamanda@gmail.com>
2020-03-18 20:22:00 +0800

    edited student f/l name to name, search companies by name, modified search students by name etc.

---
+++ source/HustlePlus/HustlePlus-war/src/java/jsf/managedBean/SearchCompaniesByNameManagedBean.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedBean;
+
+import ejb.session.stateless.CompanySessionBeanLocal;
+import entity.Company;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author amanda
+ */
+@Named(value = "searchCompaniesByNameManagedBean")
+@RequestScoped
+public class SearchCompaniesByNameManagedBean {
+
+    @EJB(name = "CompanySessionBeanLocal")
+    private CompanySessionBeanLocal companySessionBeanLocal;
+    
+    @Inject
+    private ViewCompanyManagedBean viewCompanyManagedBean;
+
+    private String searchString;
+    private List<Company> companies;
+
+    public SearchCompaniesByNameManagedBean() {
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        setSearchString((String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("companySearchString"));
+
+        if (getSearchString() == null || getSearchString().trim().length() == 0) {
+            setCompanies(companySessionBeanLocal.retrieveAllCompanies());
+        } else {
+            //try {
+                setCompanies(companySessionBeanLocal.retrieveCompaniesByName(getSearchString()));
+            //} catch (CompanyNotFoundException ex) {
+                
+         //   }
+        }
+    }
+
+    public void searchCompany() {
+        if (getSearchString() == null || getSearchString().trim().length() == 0) {
+            setCompanies(companySessionBeanLocal.retrieveAllCompanies());
+        } else {
+            //try {
+                setCompanies(companySessionBeanLocal.retrieveCompaniesByName(getSearchString()));
+            //} catch (CompanyNotFoundException ex) {
+                
+            //}
+        }
+    }
+
+    /**
+     * @return the searchString
+     */
+    public String getSearchString() {
+        return searchString;
+    }
+
+    /**
+     * @param searchString the searchString to set
+     */
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
+    }
+
+    /**
+     * @return the companies
+     */
+    public List<Company> getCompanies() {
+        return companies;
+    }
+
+    /**
+     * @param companies the companies to set
+     */
+    public void setCompanies(List<Company> companies) {
+        this.companies = companies;
+    }
+
+    /**
+     * @return the viewCompanyManagedBean
+     */
+    public ViewCompanyManagedBean getViewCompanyManagedBean() {
+        return viewCompanyManagedBean;
+    }
+
+    /**
+     * @param viewCompanyManagedBean the viewCompanyManagedBean to set
+     */
+    public void setViewCompanyManagedBean(ViewCompanyManagedBean viewCompanyManagedBean) {
+        this.viewCompanyManagedBean = viewCompanyManagedBean;
+    }
+    
+}
---
+++ source/HustlePlus/HustlePlus-war/src/java/jsf/managedBean/ViewCompanyManagedBean.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.managedBean;
+
+import entity.Company;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author amanda
+ */
+@Named(value = "viewCompanyManagedBean")
+@RequestScoped
+public class ViewCompanyManagedBean implements Serializable {
+
+    private Company companyToView;
+    
+    /**
+     * Creates a new instance of ViewCompanyManagedBean
+     */
+    public ViewCompanyManagedBean() {
+        companyToView = new Company();
+    }
+    
+    @PostConstruct
+    public void postConstruct()
+    {
+    }
+
+    /**
+     * @return the companyToView
+     */
+    public Company getCompanyToView() {
+        return companyToView;
+    }
+
+    /**
+     * @param companyToView the companyToView to set
+     */
+    public void setCompanyToView(Company companyToView) {
+        this.companyToView = companyToView;
+    }
+    
+}
