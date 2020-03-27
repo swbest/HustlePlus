da8b8f46c5d0ba530280c22f128a9af0b82e0df4
Wong Jin Fu Shaun <sw_best@hotmail.com>
2020-03-27 23:45:39 +0800

    Updated CompanyReviewSessionBean

--- source/HustlePlus/HustlePlus-ejb/src/java/ejb/session/stateless/CompanyReviewSessionBean.java
+++ source/HustlePlus/HustlePlus-ejb/src/java/ejb/session/stateless/CompanyReviewSessionBean.java
@@ -1,4 +1,4 @@
-/*
+
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -23,9 +23,9 @@ import util.exception.ReviewNotFoundException;
 import util.exception.UnknownPersistenceException;
 import util.exception.UpdateReviewException;
 
-/**
- *
- * @author sw_be
+
+ 
+  @author sw_be
  
 @Stateless
 public class CompanyReviewSessionBean implements CompanyReviewSessionBeanLocal {
@@ -109,4 +109,3 @@ public class CompanyReviewSessionBean implements CompanyReviewSessionBeanLocal {
         return msg;
     }
 }
-*/
