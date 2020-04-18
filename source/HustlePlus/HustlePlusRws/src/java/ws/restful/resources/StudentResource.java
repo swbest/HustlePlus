d30a36c5829b8e965769d798ef47f581d07cfbf4
Wong Jin Fu Shaun <sw_best@hotmail.com>
2020-04-19 04:00:48 +0800

    Added student login response to restful web services

--- source/HustlePlus/HustlePlusRws/src/java/ws/restful/resources/StudentResource.java
+++ source/HustlePlus/HustlePlusRws/src/java/ws/restful/resources/StudentResource.java
@@ -20,13 +20,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import util.exception.InvalidLoginCredentialException;
 import ws.restful.model.CreateNewStudentReq;
 import ws.restful.model.CreateNewStudentRsp;
 import ws.restful.model.ErrorRsp;
 import ws.restful.model.RetrieveAllStudentsRsp;
 import ws.restful.model.RetrieveStudentRsp;
+import ws.restful.model.StudentLoginRsp;
 
 /**
  * REST Web Service
@@ -46,10 +50,46 @@ public class StudentResource {
      */
     public StudentResource() {
     }
+    
+     /**
+     * Login for Student
+     * 
+     * @return response
+     */
+    @Path("studentLogin")
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response studentLogin(@QueryParam("username") String username, 
+                                @QueryParam("password") String password)
+    {
+        try
+        {
+            Student student = studentSessionBean.studentLogin(username, password);
+            System.out.println("********** StudentResource.studentLogin(): Student " + student.getUsername() + " login remotely via web service");
+
+            student.setPassword(null);
+            student.setSalt(null);
+            
+            return Response.status(Status.OK).entity(new StudentLoginRsp(student)).build();
+        }
+        catch(InvalidLoginCredentialException ex)
+        {
+            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
+            
+            return Response.status(Status.UNAUTHORIZED).entity(errorRsp).build();
+        }
+        catch(Exception ex)
+        {
+            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
+            
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(errorRsp).build();
+        }
+    }
 
     /**
      * Retrieves representation of an instance of
-     * ws.restful.resources.ProjectResource
+     * ws.restful.resources.StudentResource
      *
      * @return an instance of java.lang.String
      */
