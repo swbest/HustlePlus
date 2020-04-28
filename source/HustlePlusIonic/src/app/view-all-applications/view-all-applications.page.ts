abfa8572c8ddbb7863d1c631c149ce0ebc571371
Damien Tan <dtjldamien@gmail.com>
2020-04-28 18:16:11 +0800

    completed view my application, might need some touch up for ui

--- source/HustlePlusIonic/src/app/view-all-applications/view-all-applications.page.ts
+++ source/HustlePlusIonic/src/app/view-all-applications/view-all-applications.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Application } from '../application';
+import { ApplicationService } from '../application.service';
 
 @Component({
   selector: 'app-view-all-applications',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewAllApplicationsPage implements OnInit {
 
-  constructor() { }
+  applications: Application[];
+  errorMessage: string;
+
+  constructor(private router: Router,
+    private applicationService: ApplicationService) { }
 
   ngOnInit() {
+    this.refreshApplications();
   }
 
+  ionViewWillEnter() {
+    this.refreshApplications();
+  }
+
+  refreshApplications() {
+    this.applicationService.getMyApplications().subscribe(
+      response => {
+        this.applications = response.applications
+      },
+      error => {
+        this.errorMessage = error
+        console.log('********** ViewAllApplicationsPage.ts: ' + error);
+      }
+    );
+  }
+
+  getSearchApplications(ev: any) {
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.applications = this.applications.filter((application) => {
+        return (application.project.projectName.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  onCancel(ev: any) {
+    // Reset items back to all of the items
+    this.refreshApplications();
+  }
 }
